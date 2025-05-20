4ec78dd6e70b21b0ddd660d97057695a3747a532
SUBcharacter <farg557@gmail.com>
2025-05-20 15:32:04 +0900

    2025.05.20 프로그램 생성

---
+++ C/C/Source.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//진입점
+void main()
+{
+	// main 함수는 프로그램의 진입점 역할을 하는 함수입니다.
+	//printf("I need Monster Energy...");
+
+#pragma region 자료형
+	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+	
+
+	// 자료형은 각각의 자료형 마다 크기가 정해져 있으며, 자료형의 크기는
+	// 바이트 단위로 이루어집니다.
+	
+
+	//자료형의 경우 자료형에 따라 저장가능 값의 종류와 범위가 결정됩니다.
+
+#pragma endregion
+
+
+#pragma region 변수
+	// 데이터를 저장할 수 있는 메모리 공간을 
+	// 생성하는 것 입니다.
+
+	char alphabet = 'A';
+	int integer = 100;
+	float realNum = 36.5f;
+
+	// 변수의 메모리 공간은 프로그램이 실행될 때마다 바뀐다.
+
+#pragma endregion
+
+	
+
+	
+}
