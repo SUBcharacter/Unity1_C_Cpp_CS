5283c6204d0d5ff14e2e08cbf20b143aacd33bd9
SUBcharacter <farg557@gmail.com>
2025-05-30 15:45:11 +0900

    2025.05.30 함수

--- C/C/Source.c
+++ C/C/Source.c
@@ -4,115 +4,89 @@
 #include <stdlib.h>
 #include <time.h>
 
-void main()
+void Process(int* number)
 {
-#pragma region 배열
-
-	// 같은 자료형의 변수들로 이루어진 유한 집합
-	// 배열의 경우 첫 번째 원소는 0부터 시작	
-
-	//int array[1000];
-	//int count = sizeof(array) / sizeof(int);
-	//printf("배열의 크기 : %d\n\n", count);
-	//
-	//for (int i = 1; i <= count; i++)
-	//{
-	//	array[i-1] = 10 * i;
-	//	printf("대입 x-%d : %d\n", i, array[i-1]);
-	//}
+	printf("Process 함수 호출됨.\n\n");
+	printf("현재 숫자 : %d\n", *number);
+	*number += 30;
+	printf("숫자 변환 됨\n\n");
 	
-	// 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
-	// 배열의 크기는 컴파일 되는 시점부터 고정된 메모리 공간을 가지게 된다
+}
 
-	//int list[5] = { 1, 2, 3, 4, 5 };
-	//
-	//int* pointer = list;
-	//printf("list[0] : %d\n", list[0]);
-	//printf("list[0] address : %p\n", pointer);
-	//*pointer = 99;
-	//
-	//printf("list[0] : %d\n\n", list[0]);
-	//
-	//
-	//printf("list[1] : %d\n", list[1]);
-	//printf("list[1] address : %p\n", pointer+1);
-	//pointer++;
-	//*pointer = 100;
-	//
-	//printf("list[1] : %d\n", list[1]);
+void Stat(int health, const char* name)
+{
+	printf("닉네임 : %s\n\n", name);
+	printf("체력 : %d/100\n\n\n", health);
+}
 
-	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서 설정한 요소에 따라 배열의 크기가 결정됨
+void Switching1(int a, int b)
+{
+	printf("교환 전 숫자 | a = %d, b = %d\n\n", a, b);
+	int temp = b;
+	b = a;
+	a = temp;
 
-	//float container[ ] = { 10.5f,20.8f,35.6f,31.4f };
-	
-
-#pragma endregion
-
-#pragma region 문자열
-
-	// 연속적인 메모리 공간에 저장된 문자 변수의 집합
-	// 문자열의 경우 포인터를 이용하여
-	// 문자열 상수를 가리키도록 할 수 있으며,
-	// 문자열 상수는 데이터 영역에 읽기 전용 공간에 저장되기 때문에,
-	// 문자열의 값을 변경할 수 없다
-
-	//const char* Title = "Call of Duty : Modern Warfare";
-	//printf("%s\n", Title);
-
-	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 결정되며,
-	// 마지막에 문자열의 끝을 알려주는 제어 문자가 포함된다
-
-	//Title = "Battle Field";
-	
-	//printf("%s\n", Title);
-
-	// 문자열을 저장하게 되면 맨 마지막에 무효의 문자까지 메모리 공간에 저장된다
-
-	//char name[] = "Bob Robert";
-	//char* const name2 = "Bob Robert";
-	//printf("%s\n", name);
-	//
-	//name[0] = 'J';
-	//name[1] = 'i';
-	//name[2] = 'm';
-	//
-	//printf("%s\n", name);
-
-#pragma endregion
-
-#pragma region 과제
-
-	/*
-	* 아스키 코드, 유니코드 조사 및 활용
-	*/
-
-	// 아스키코드(American Standard Code for Information Interchange)
-	// 미국정보교환표준부호
-	// 다양한 문자들을 0~127까지의 숫자로 표현할 수 있게 만든 것
-	// C언어에서 문자를 표현할 때 주로 사용된다.
-	// Ex.
-
-	int character = 'A';
-
-	printf("아스키 코드 숫자 : %d\n", character);
-	printf("아스키 코드 문자 : %c\n", character);
-
-	// 표현되는건 문자이지만, 숫자와도 연산할 수 있다.
-	// Ex.
-	
-	character += 1;
-	printf("아스키 코드 숫자 : %d\n", character);
-	printf("아스키 코드 문자 : %c\n", character);
-	//---------------------------------------------------------
-
-	// 유니코드
-	// 전세계 모든 문자를 일관되게 표현할 수 있도록 설계된 산업 표준
-	// C언어에서는 주로 사용되지 않지만 출력할 수 있는 방법은 있다.
-	// Ex.
-	
-	printf("유니코드(A) : \u0041");
-
-#pragma endregion
-
+	printf("교환 된 숫자 | a = %d, b = %d\n\n\n", a, b);
 
 }
+
+void Switching2(int* a, int* b)
+{
+	int temp = *b;  // 매개변수 b에 들어온 주소를 역참조한 값을 temp에 저장
+
+	*b = *a;  // 매개변수 a의 역참조 값을 매개변수 b의 역참조 값으로 대입
+	*a = temp; // 매개변수 a의 역참조 값에 temp(b의 역참조 값)의 값을 대입
+}
+
+void main()
+{
+#pragma region 함수
+
+	// 하나의 특별한 목적의 작업을 수행하기 위해
+	// 독립적으로 설계된 코드의 집합
+	// 함수는 프로그램이 실행 중일 때 함수 호출 위치에서
+	// 해당 함수로 이동시킨 두, 작업을 수행하고
+	// 다시 원래 위치로 돌아오는 구조
+	//int testing = 10;
+	//printf("testing : %d\n",testing);
+	//
+	//Process(&testing);
+	//
+	//printf("testing : %d\n",testing);
+#pragma endregion
+
+#pragma region 매개변수
+
+	// 함수의 정의에서 전달받은인수를 함수 내부로
+	// 전달하기 위해 사용하는 변수
+	//매개 변수는 함수 내부에서만연산이 이루어지며,
+	// 여러개의 매백 변수르 생성할 수 있음
+
+	//int health = 80;
+	//char* name = "Goblin";
+	//
+	//Stat(health, name);
+	//Stat(90, "Orc");
+	//Stat(100, "Fenrir");
+
+#pragma endregion
+
+#pragma region 인수
+
+	// 함수가 호출될 때 매개 변수에 실제로 전달되는 값
+	// 인수의 경우 값을 전달하는 인수와 값을 전달받는 매개 변수의 자료형이 서로 일치해야 함
+
+	Switching1(30, 10);
+
+	int x = 100;
+	int y = 50;
+
+	printf("교환 전 숫자 : x = %d, y = %d\n\n", x, y); // 교환 전 숫자를 출력
+
+	Switching2(&x, &y);  // 매개 변수로 x와 y의 주소값을 전달
+
+	printf("교환 후 숫자 : x = %d, y = %d\n\n", x, y); // 교환 후 숫자를 출력
+#pragma endregion
+
+}
+
---
+++ C/Class7th (Arrangement)/Source.c
@@ -0,0 +1,162 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+#pragma region 배열
+
+	// 같은 자료형의 변수들로 이루어진 유한 집합
+	// 배열의 경우 첫 번째 원소는 0부터 시작	
+
+	//int array[1000];
+	//int count = sizeof(array) / sizeof(int);
+	//printf("배열의 크기 : %d\n\n", count);
+	//
+	//for (int i = 1; i <= count; i++)
+	//{
+	//	array[i-1] = 10 * i;
+	//	printf("대입 x-%d : %d\n", i, array[i-1]);
+	//}
+
+	// 배열은 원하는 원소에 원하는 값을 저장할 수 있으며,
+	// 배열의 크기는 컴파일 되는 시점부터 고정된 메모리 공간을 가지게 된다
+
+	//int list[5] = { 1, 2, 3, 4, 5 };
+	//
+	//int* pointer = list;
+	//printf("list[0] : %d\n", list[0]);
+	//printf("list[0] address : %p\n", pointer);
+	//*pointer = 99;
+	//
+	//printf("list[0] : %d\n\n", list[0]);
+	//
+	//
+	//printf("list[1] : %d\n", list[1]);
+	//printf("list[1] address : %p\n", pointer+1);
+	//pointer++;
+	//*pointer = 100;
+	//
+	//printf("list[1] : %d\n", list[1]);
+
+	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서 설정한 요소에 따라 배열의 크기가 결정됨
+
+	//float container[ ] = { 10.5f,20.8f,35.6f,31.4f };
+
+
+#pragma endregion
+
+#pragma region 문자열
+
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합
+	// 문자열의 경우 포인터를 이용하여
+	// 문자열 상수를 가리키도록 할 수 있으며,
+	// 문자열 상수는 데이터 영역에 읽기 전용 공간에 저장되기 때문에,
+	// 문자열의 값을 변경할 수 없다
+
+	//const char* Title = "Call of Duty : Modern Warfare";
+	//printf("%s\n", Title);
+
+	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기가 결정되며,
+	// 마지막에 문자열의 끝을 알려주는 제어 문자가 포함된다
+
+	//Title = "Battle Field";
+
+	//printf("%s\n", Title);
+
+	// 문자열을 저장하게 되면 맨 마지막에 무효의 문자까지 메모리 공간에 저장된다
+
+	//char name[] = "Bob Robert";
+	//char* const name2 = "Bob Robert";
+	//printf("%s\n", name);
+	//
+	//name[0] = 'J';
+	//name[1] = 'i';
+	//name[2] = 'm';
+	//
+	//printf("%s\n", name);
+
+#pragma endregion
+
+#pragma region 과제
+
+	/*
+	* 아스키 코드, 유니코드 조사 및 활용
+	*/
+
+	// 아스키코드(American Standard Code for Information Interchange)
+	// 미국정보교환표준부호
+	// 다양한 문자들을 0~127까지의 숫자로 표현할 수 있게 만든 것
+	// C언어에서 문자를 표현할 때 주로 사용된다.
+	// Ex.
+
+	//int character = 'A';
+	//
+	//printf("아스키 코드 숫자 : %d\n", character);
+	//printf("아스키 코드 문자 : %c\n", character);
+
+	// 표현되는건 문자이지만, 숫자와도 연산할 수 있다.
+	// Ex.
+
+	//character += 1;
+	//printf("아스키 코드 숫자 : %d\n", character);
+	//printf("아스키 코드 문자 : %c\n", character);
+	//---------------------------------------------------------
+
+	// 유니코드
+	// 전세계 모든 문자를 일관되게 표현할 수 있도록 설계된 산업 표준
+	// C언어에서는 주로 사용되지 않지만 출력할 수 있는 방법은 있다.
+	// Ex.
+
+	//printf("유니코드(A) : \u0041");
+
+#pragma endregion
+
+#pragma region (2)차원 배열
+
+	// 배열의 요소로 또 다른 배열을 가지는 배열
+	// 2차원 배열은 행과 열로 구분되며,
+	// 앞에 있는 배열은 열을 의미하고, 뒤에 있는 배열은 행을 의미한다
+
+//int matrix[3][3] =
+//{
+//	{1, 2, 3},
+//	{4, 5, 6},
+//	{7, 8, 9}
+//};
+//
+//for (int i = 0; i < 3; i++)
+//{
+//	for (int j = 0; j < 3; j++)
+//	{
+//		printf("%d ", matrix[i][j]);
+//	}
+//	printf("\n");
+//}
+
+//int matrix[3][3];
+//int count = 0;
+//for (int i = 0; i < 3; i++)
+//{
+//	for (int j = 0; j < 3; j++)
+//	{
+//		count++;
+//		matrix[i][j] = count;
+//	}
+//}
+//
+//for (int i = 0; i < 3; i++)
+//{
+//	for (int j = 0; j < 3; j++)
+//	{
+//		printf("%d ", matrix[i][j]);
+//	}
+//	printf("\n");
+//}
+
+
+#pragma endregion
+
+}
