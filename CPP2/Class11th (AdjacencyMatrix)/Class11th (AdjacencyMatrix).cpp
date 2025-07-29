541a8ccfd56cdd3c0f6f421864eba6177d5a17d4
SUBcharacter <farg557@gmail.com>
2025-07-29 15:39:29 +0900

    2025.07.29 인접 리스트

---
+++ CPP2/Class11th (AdjacencyMatrix)/Class11th (AdjacencyMatrix).cpp
@@ -0,0 +1,180 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define SIZE 6
+
+using namespace std;
+
+template<typename T>
+class AdjacencyMatrix
+{
+private:
+    int vertice; // 정점의 개수
+    int capacity; // 최대 용량
+    T* vertex;
+    int** matrix;
+
+
+public:
+    AdjacencyMatrix()
+    {
+        vertice = 0;
+        capacity = 0;
+        vertex = nullptr;
+        matrix = nullptr;
+
+    }
+
+    void InitResize()
+    {
+        matrix = new int* [vertice];
+        for (int i = 0; i < vertice; i++)
+        {
+            matrix[i] = new int[vertice];
+            for (int j = 0; j < vertice; j++)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    void resizeMat(int newCap)
+    {
+        int** newMat = new int* [newCap];
+        for (int i = 0; i < newCap; i++)
+        {
+            newMat[i] = new int[newCap];
+            for (int j = 0; j < newCap; j++)
+            {
+                if (matrix != nullptr && i < vertice && j < vertice)
+                {
+                    newMat[i][j] = matrix[i][j];
+                }
+                else
+                {
+                    newMat[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix != nullptr)
+        {
+            for (int i = 0; i < vertice; i++)
+            {
+                delete[] matrix[i];
+            }
+            delete[] matrix;
+        }
+
+        matrix = newMat;
+    }
+
+    void resize(int newSize)
+    {
+        T* newData = new T[newSize];
+
+        int limit = (newSize < vertice) ? newSize : vertice;
+        for (int i = 0; i < limit; i++)
+        {
+            newData[i] = vertex[i];
+        }
+
+        delete[] vertex;
+        vertex = newData;
+        capacity = newSize;
+
+
+    }
+
+
+
+    void push(T data)
+    {
+        if (capacity <= 0)
+        {
+            resize(2);
+            resizeMat(2);
+        }
+        else if (vertice >= capacity)
+        {
+            int newCap = capacity * 2;
+            resize(newCap);
+            resizeMat(newCap);
+        }
+
+        vertex[vertice++] = data;
+
+    }
+
+    void edge(int i, int j)
+    {
+        if (vertice == 0 || vertex == nullptr)
+        {
+            cout << "Warning : No Exist Vertice" << endl;
+            return;
+        }
+        if (i >= vertice || j >= vertice)
+        {
+            cout << "Warning : Out of Range Vertice" << endl;
+            return;
+        }
+
+        matrix[i][j] = 1;
+        matrix[j][i] = 1;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < vertice; i++)
+        {
+            for (int j = 0; j < vertice; j++)
+            {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    friend ostream& operator<<(ostream& ostream, const AdjacencyMatrix<T>& admat)
+    {
+        for (int i = 0; i < admat.vertice; i++)
+        {
+            for (int j = 0; j < admat.vertice; j++)
+            {
+                ostream << admat.matrix[i][j] << " ";
+            }
+            ostream << endl;
+        }
+
+        return ostream;
+    }
+
+    ~AdjacencyMatrix()
+    {
+        if (matrix != nullptr)
+        {
+            for (int i = 0; i < capacity; i++)
+            {
+                delete[] matrix[i];
+            }
+            delete[] matrix;
+        }
+        delete[] vertex;
+    }
+
+};
+
+int main()
+{
+    AdjacencyMatrix<int> adj;
+
+    adj.push(5);
+    adj.push(7);
+    adj.push(12);
+
+    adj.edge(0, 1);
+    adj.edge(0, 2);
+
+
+    cout << adj;
+    return 0;
+}
--- CPP2/Program/Program.cpp
+++ CPP2/Program/Program.cpp
@@ -1,151 +1,97 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#define SIZE 6
+#define SIZE 10
 
 using namespace std;
 
 template<typename T>
-class AdjacencyMatrix
+class AdjacencyList
 {
 private:
-    int vertice; // 정점의 개수
-    int capacity; // 최대 용량
-    T* vertex;
-    int** matrix;
+    struct Node
+    {
+        int index;
+        Node* next;
 
+        Node(int i, Node* n = nullptr) 
+        {
+            this->index = i;
+            next = n;
+        }
+    };
+
+    int vertice; // 정점의 개수
+    T vertex[SIZE]; // 정점의 집합
+    Node* list[SIZE]; // 인접 리스트
 
 public:
-    AdjacencyMatrix()
+    AdjacencyList()
     {
         vertice = 0;
-        capacity = 0;
-        vertex = nullptr;
-        
-    }
-
-    void InitResize()
-    {
-        matrix = new int* [vertice];
-        for (int i = 0; i < vertice; i++)
+        for (int i = 0; i < SIZE; i++)
         {
-            matrix[i] = new int[vertice];
-            for (int j = 0; j < vertice; j++)
-            {
-                matrix[i][j] = 0;
-            }
+            list[i] = NULL;
+            vertex[i] = NULL;
         }
     }
-
-    void resize()
-    {
-        int** newMat = new int* [vertice];
-        for (int i = 0; i < vertice; i++)
-        {
-            newMat[i] = new int[vertice];
-            for (int j = 0; j < vertice; j++)
-            {
-                if (matrix != nullptr && i < vertice-1 && j < vertice-1)
-                {
-                    newMat[i][j] = matrix[i][j];
-                }
-                else
-                {
-                    newMat[i][j] = 0;
-                }
-            }
-        }
-        
-        if (matrix != nullptr)
-        {
-            for (int i = 0; i < vertice; i++)
-            {
-                delete[] matrix[i];
-            }
-            delete[] matrix;
-        }
-
-        matrix = newMat;
-    }
-
-    void resize(int newSize)
-    {
-        T* newData = new T[newSize];
-
-        int limit = (newSize < vertice) ? newSize : vertice;
-        for (int i = 0; i < limit; i++)
-        {
-            newData[i] = vertex[i];
-        }
-
-        delete[] vertex;
-        vertex = newData;
-        capacity = newSize;
-
-        
-    }
-
-
 
     void push(T data)
     {
-        if (capacity <= 0)
+        if (vertice >= SIZE)
         {
-            resize(1);
+            cout << "Warning : contact out of range" << endl;
+            return;
         }
-        else if (vertice >= capacity)
-        {
-            resize(capacity * 2);
-        }
-
-        vertex[vertice++] = data;
-        resize();
+        vertex[vertice] = data;
+        list[vertice] = nullptr;
+        vertice++;
     }
 
     void edge(int i, int j)
     {
-        if (vertice == 0 || vertex == nullptr)
+        if (size <= 0)
         {
-            cout << "Warning : No Exist Vertice" << endl;
+            cout << "adjacency list is empty" << endl;
+        }
+        else if (i >= vertice || j >= vertice)
+        {
+            cout << "Warning : Invalied vertex index" << endl;
             return;
         }
-        if (i >= vertice || j >= vertice)
+        else
         {
-            cout << "Warning : Out of Range Vertice" << endl;
-            return;
+            list[i] = new Node(j, list[i]);
+            list[j] = new Node(i, list[j]);
         }
-        
-        resize();
-
-        matrix[i][j] = 1;
-        matrix[j][i] = 1;
     }
 
-    void print()
+    ~AdjacencyList()
     {
-        for (int i = 0; i < vertice; i++)
+        for (int i = 0; i < SIZE; i++)
         {
-            for (int j = 0; j < vertice; j++)
+            Node* current = list[i];
+            while (current != nullptr)
             {
-                cout << matrix[i][j] << " ";
+                Node* temp = current;
+                current = current->next;
+                delete temp;
             }
-            cout << endl;
+            list[i] = nullptr;
         }
     }
-
 };
 
 int main()
 {
-    AdjacencyMatrix<int> adj;
+    AdjacencyList<int> adl;
 
-    adj.push(5);
-    adj.push(7);
-    adj.push(12);
+    adl.push(5);
+    adl.push(7);
+    adl.push(12);
 
-    adj.edge(0, 1);
-    adj.edge(0, 2);
+    adl.edge(0, 2);
+    adl.edge(0, 1);
 
 
-    adj.print();
     return 0;
 }
