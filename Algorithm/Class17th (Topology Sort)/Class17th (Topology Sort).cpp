2654ba0dc0b520ebdaea6e44ab5922b0dd90c3e1
SUBcharacter <farg557@gmail.com>
2025-08-19 15:42:45 +0900

    2025.08.19 서로소 집합

---
+++ Algorithm/Class17th (Topology Sort)/Class17th (Topology Sort).cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+#include <queue>
+#define SIZE 8
+
+using namespace std;
+
+class Graph
+{
+private:
+	vector<int> list[SIZE];
+	int indegree[SIZE];
+
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			indegree[i] = 0;
+		}
+	}
+
+	void insert(int vertex, int edge)
+	{
+		list[vertex].push_back(edge);
+		indegree[edge]++;
+	}
+
+	void search()
+	{
+		queue<int> q;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (indegree[i] == 0)
+			{
+				q.push(i);
+			}
+		}
+
+		while (!q.empty())
+		{
+			int current = q.front();
+
+			q.pop();
+
+			cout << current << " ";
+
+			for (int next : list[current])
+			{
+				indegree[next]--;
+				if (indegree[next] == 0)
+				{
+					q.push(next);
+				}
+			}
+		}
+	}
+
+};
+
+
+int main()
+{
+#pragma region 위상 정렬
+
+	// 병합 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	// 모든 정점을 차례대로 진행하는 알고리즘
+
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없다.
+
+	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
+
+	// 시간 복잡도 : 0(V + E)
+
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입
+
+	// 2. Queue에서 원소를 꺼내 연결된 모든
+
+
+	Graph graph;
+
+	graph.insert(1, 2);
+	graph.insert(1, 5);
+
+	graph.insert(2, 3);
+	graph.insert(3, 4);
+	graph.insert(4, 6);
+
+	graph.insert(5, 6);
+	graph.insert(6, 7);
+
+	graph.search();
+
+#pragma endregion
+
+	return 0;
+}
---
+++ Algorithm/Class18th (Union Find)/Class18th (Union Find).cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+#include <queue>
+#define SIZE 7
+
+using namespace std;
+
+int parent[SIZE];
+
+int find(int x)
+{
+	if (parent[x] != x)
+	{
+		parent[x] = find(parent[x]);
+	}
+	return parent[x];
+}
+
+void Union(int x, int y)
+{
+	int rootX = find(x);
+	int rootY = find(y);
+
+	if (rootX > rootY)
+	{
+		parent[rootX] = rootY;
+	}
+	else if (rootX < rootY)
+	{
+		parent[rootY] = rootX;
+	}
+}
+
+bool Same(int x, int y)
+{
+	if (find(x) != find(y))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+#pragma region 서로소 집합
+
+	// 서로 공통된 원소를 가지고 있지 않은 두 개 이상의 집합
+
+	// union (합집합) : 두 집합을 하나로 합치는 연산
+
+	// find (찾기) : 특정한 원소가 속한 집합이 어떤 집합인지 알려주는 연산
+
+	// 1. 합집합 연산을 확인하여, 서로 연결된 두 노드를 확인한다.
+	//	1) A와 B의 루트 노드를 통해 A'과 B'을 각각 찾는다.
+	//	2) A'과 B'의 부모 노드를 설정한다.
+
+	// 2. 모든 합집합 연산을 처리할 때까지 1번 과정을 반복한다.
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		parent[i] = i;
+	}
+
+	for (int p : parent)
+	{
+		cout << p << " ";
+	}
+
+	cout << endl;
+
+	Union(0, 1);
+	Union(1, 2);
+	Union(3, 0);
+	Union(4, 5);
+
+	for (int p : parent)
+	{
+		cout << p << " ";
+	}
+
+	cout << endl << (Same(1, 4) ? "true" : "false");
+
+#pragma endregion
+
+	return 0;
+}
