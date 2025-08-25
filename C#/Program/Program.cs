fe207f513af2499a5acb892a7829d08eb1633300
SUBcharacter <farg557@gmail.com>
2025-08-25 15:41:30 +0900

    2025.08.25 C# 시작

---
+++ C#/Program/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region 박싱
+
+            // 값 형식의 데이터를 참조 형식으로 변환하는 과정
+
+            //int value = 100;
+            //
+            //object generic = value;
+            //
+            //Console.WriteLine(value);
+            //Console.WriteLine(generic);
+
+            #endregion
+
+            #region 언박싱
+
+            // 참조 형식의 데이터를 값 형식으로 변환하는 과정
+
+            //int box = (int)generic;
+            //
+            //Console.WriteLine(box);
+
+            #endregion
+
+            Utility util = new Utility();
+
+            util.Pause();
+
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine($"{x} {y}");
+
+            util.Swap(ref x, ref y);
+
+            Console.WriteLine($"{x} {y}");
+
+            int abs = 10;
+
+            util.Absolute(in abs);
+        }
+    }
+}
---
+++ C#/Program/Utility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Utility
+    {
+        private int data;
+
+        public void Pause()
+        {
+            Console.WriteLine("PAUSE");
+        }
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public void Absolute(in int value)
+        {
+            if(value >= 0 )
+            {
+                Console.WriteLine($"ABS : {value}");
+            }
+            else
+            {
+                Console.WriteLine($"ABS : {-value}");
+            }
+
+        }
+    }
+}
